309ac9f425d3a573419dedb73185a60a1285f5cc
CaptnB <captnb@gmail.com>
2013-03-11 20:35:09 -0400

    Added speed bonus

--- src/com/pathfindersdkutils/main/UtilsApp.java
+++ src/com/pathfindersdkutils/main/UtilsApp.java
@@ -3,7 +3,7 @@ package com.pathfindersdkutils.main;
 import com.pathfindersdk.basics.CreatureType;
 import com.pathfindersdk.bonus.InitiativeBonus;
 import com.pathfindersdk.bonus.SaveBonus;
-import com.pathfindersdk.bonus.SizeBonus;
+import com.pathfindersdk.bonus.SpeedBonus;
 import com.pathfindersdk.books.builders.AdvancedPlayersGuideBuilder;
 import com.pathfindersdk.books.builders.AdvancedRaceGuideBuilder;
 import com.pathfindersdk.books.builders.Bestiary2Builder;
@@ -14,12 +14,13 @@ import com.pathfindersdk.books.builders.UltimateCombatBuilder;
 import com.pathfindersdk.books.builders.UltimateMagicBuilder;
 import com.pathfindersdk.books.items.Race;
 import com.pathfindersdk.creatures.Character;
-import com.pathfindersdk.enums.AbilityType;
 import com.pathfindersdk.enums.BonusType;
 import com.pathfindersdk.enums.CreatureMainType;
 import com.pathfindersdk.enums.CreatureSubtype;
 import com.pathfindersdk.enums.SaveType;
 import com.pathfindersdk.enums.SizeType;
+import com.pathfindersdk.enums.SpeedType;
+import com.pathfindersdk.stats.Speed;
 import com.pathfindersdkutils.json.BookJson;
 
 
@@ -54,9 +55,11 @@ public class UtilsApp
   
   private static void testCharacter()
   {
+    // Name
     Character character = new Character();
     character.setName("Gimli of the Deep");
     
+    // Ability Scores
     character.getStrenght().setBaseScore(7);
     character.getDexterity().setBaseScore(17);
     character.getConstitution().setBaseScore(12);
@@ -64,18 +67,25 @@ public class UtilsApp
     character.getWisdom().setBaseScore(8);
     character.getCharisma().setBaseScore(18);
     
+    // Race
     CreatureType creatureType = new CreatureType(CreatureMainType.HUMANOID);
     creatureType.addSubtype(CreatureSubtype.DWARF);
     creatureType.addSubtype(CreatureSubtype.EARTH);
     
     Race race = new Race("Earth Dwarf", creatureType, SizeType.MEDIUM);
     race.addBonus(new InitiativeBonus(2, BonusType.MORALE));
-    race.addBonus(new SaveBonus(1, SaveType.FORT, BonusType.RACIAL, "against poison"));
-    race.addBonus(new SaveBonus(2, SaveType.FORT, BonusType.LUCK, "when upside down"));
-    race.addBonus(new SaveBonus(1, SaveType.REF, BonusType.RACIAL));
-    race.addBonus(new SaveBonus(2, SaveType.REF, BonusType.RACIAL));
-    race.addBonus(new SaveBonus(1, SaveType.REF, BonusType.MORALE));
+    race.addBonus(new SaveBonus(1, BonusType.RACIAL, SaveType.FORT, "against poison"));
+    race.addBonus(new SaveBonus(2, BonusType.LUCK, SaveType.FORT, "when upside down"));
+    race.addBonus(new SaveBonus(1, BonusType.RACIAL, SaveType.REF));
+    race.addBonus(new SaveBonus(2, BonusType.RACIAL, SaveType.REF));
+    race.addBonus(new SaveBonus(1, BonusType.MORALE, SaveType.REF));
+    
+    race.addSpeed(new Speed(20, SpeedType.BASE));
+    race.addSpeed(new Speed(20, SpeedType.ARMOR));
+    race.addSpeed(new Speed(5,  SpeedType.BURROW));
     character.setRace(race);
+    character.getSpeed(SpeedType.ARMOR).addBonus(new SpeedBonus(-5, BonusType.RACIAL, SpeedType.ARMOR, "for being dumb"));
+    character.getSpeed(SpeedType.BURROW).addBonus(new SpeedBonus(2, BonusType.ENHANCEMENT, SpeedType.BURROW));
     
     System.out.println("Name : " + character.getName());
     System.out.println("Race : " + character.getRace().toString());
@@ -91,5 +101,8 @@ public class UtilsApp
     System.out.println("Reflex : " + character.getReflex().toString());
     System.out.println("Will : " + character.getWill().toString());
     System.out.println("Initiative : " + character.getInitiative().toString());
+    System.out.println("Base speed : " + character.getSpeed(SpeedType.BASE).toString());
+    System.out.println("Armor speed : " + character.getSpeed(SpeedType.ARMOR).toString());
+    System.out.println("Burrow speed : " + character.getSpeed(SpeedType.BURROW).toString());
   }
 }
